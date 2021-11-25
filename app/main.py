d19fe1c002632a220e574ad89a4c04b91401e544
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-11-25 16:35:42 -0500

    Se crea un directorio donde se declaran las constates que se usaran en el archivo principal

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import toga
+from const import *
 
 class Pokedex(toga.App):
     def __init__(self, title, id):
         toga.App.__init__(self, title, id)
 
         self.title = title
+        self.size = (WIDTH, HEIGHT)
 
     def startup(self):
-        self.main_window = toga.MainWindow('main', title=self.title, size=(400, 500))
+        self.main_window = toga.MainWindow('main', title=self.title, size=(self.size))
 
         box = toga.Box()
 
