59914d1dced4ab4d1c782c148d352658dd2c426d
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-11-25 16:21:35 -0500

    Agregasmo nuestro primer boton

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import toga
 
+def on_press(widget):
+    print("Hola mundo desde Toga")
+
+
+def new_button():
+    button = toga.Button('Hola mundo', on_press=on_press)
+    button.style.padding = 20
+    button.style.font_size = 14
+    button.style.width = 200
+    return button
+
 def build(app):
     box = toga.Box()
+
+    button1 = new_button()
+    box.add(button1)
     return box
 
 def main():
