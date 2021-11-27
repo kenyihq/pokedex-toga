fc59003e4fbe9cc44b330a99fbeb4be876b07de5
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-11-26 21:10:13 -0500

    Se integra la API a nuestra app

--- app/const.py
+++ app/const.py
@@ -1,2 +1,10 @@
+import os
+
 WIDTH = 600
 HEIGHT = 500
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ICON_DIR = os.path.join(BASE_DIR, 'icons')
+
+BULBASAUR_ICON = os.path.join(ICON_DIR, 'bulbasaur.png')
+CHARMANDER_ICON = os.path.join(ICON_DIR, 'charmander.png')
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from requests.models import RequestField
 import toga
 import requests
+import threading
+
 from const import *
 
 class Pokedex(toga.App):
@@ -10,10 +13,13 @@ class Pokedex(toga.App):
         self.size = (WIDTH, HEIGHT)
 
         self.heading = ['Name']
-        self.data = []
+        self.data = list()
+
+        self.offset = 0
 
         self.create_elements()
-        self.load_data()
+        self.load_async_data()
+        self.validate_previous_command()
 
     def startup(self):
         self.main_window = toga.MainWindow('main',title=self.title, size=(self.size))
@@ -24,19 +30,67 @@ class Pokedex(toga.App):
         split.content = [self.table, box]
 
         self.main_window.content = split
+        self.main_window.toolbar.add(self.previous_command, self.next_command)
 
         self.main_window.show()
 
     def create_elements(self):
         self.create_table()
+        self.create_toolbar()
+
+    def create_toolbar(self):
+        self.create_next_command()
+        self.create_previous_command()
+
+    def create_next_command(self):
+        self.next_command = toga.Command(self.next, label='Next', icon=BULBASAUR_ICON)
+
+    def create_previous_command(self):
+        self.previous_command = toga.Command(self.previous, label='Previous', icon=CHARMANDER_ICON)
 
 
     def create_table(self):
         self.table = toga.Table(self.heading, data=self.data, on_select = self.select_element)
 
+    def load_async_data(self):
+        self.data.clear()
+        self.table.data = self.data
 
-    def load_data(self):
-        path = 'https://pokeapi.co/api/v2/pokemon-form?offset=0&limit=20'
+        thread = threading.Thread(target=self.load_data())
+        thread.start()
+        thread.join()
+
+        self.table.data = self.data
+
+    def load_async_pokemon(self, pokemon):
+        thread = threading.Thread(target=self.load_pokemon, args=[pokemon])
+        thread.start()
+
+    def load_pokemon(self, pokemon):
+        path = f'https://pokeapi.co/api/v2/pokemon/{pokemon}/'
+
+        response = requests.get(path)
+
+        if response:
+            result = response.json()
+
+            name = result['forms'][0]['name']
+            abilities = list()
+
+            for ability in result['abilities']:
+                name_ability = ability['ability'] ['name']
+                abilities.append(name_ability)
+
+
+            sprite = result['sprites']['front_default']
+
+            print(name)
+            print(abilities)
+            print(sprite)
+
+    def load_data(self):        
+
+        path = f'https://pokeapi.co/api/v2/pokemon-form?offset={self.offset}&limit=20'
 
         response = requests.get(path)
 
@@ -44,14 +98,32 @@ class Pokedex(toga.App):
             result = response.json()
             for pokemon in result['results']:
                 name = pokemon['name']
-                self.data.append(name)
-
-        self.table.data = self.data
+                self.data.append(name)        
 
     #Callback
 
+    def next(self, widget):
+        self.offset += 1
+
+        self.handler_command(widget)
+
+    def previous(self, widget):
+        self.offset -= 1
+
+        self.handler_command(widget)
+
+    def handler_command(self, widget):
+        widget.enabled = False
+        self.load_async_data()
+        widget.enabled = True
+        self.validate_previous_command()
+
+    def validate_previous_command(self):
+        self.previous_command.enabled = not self.offset == 0
+
     def select_element(self, widget, row):
-        print(row.name)
+        if row:
+            self.load_async_pokemon(row.name)
 
 if __name__ == '__main__':
     pokedex = Pokedex('Pokedex', 'com.tutankadev.pokedex')
